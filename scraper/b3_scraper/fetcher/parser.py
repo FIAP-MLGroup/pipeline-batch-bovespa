778420e4b0f2b39a1726da30cb69064fed5988f0
João Paulo Maida <jpmaida@gmail.com>
2025-08-01 18:24:38 -0300

    Adição de coluna de data

--- scraper/b3_scraper/fetcher/parser.py
+++ scraper/b3_scraper/fetcher/parser.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 
 def normalize_data(data):
     results = data.get("results", [])
@@ -7,10 +8,15 @@ def normalize_data(data):
 
     df = pd.json_normalize(results)
 
+    # Adiciona a data do pregão como coluna
+    raw_date = data.get("header", {}).get("date")
+    df["data_corrente"] = datetime.strptime(raw_date, "%d/%m/%y").strftime("%Y-%m-%d")
+    print(df)
+
     # Remover as colunas indesejadas se existirem
     columns_to_drop = ["segment", "partAcum"]
     existing_cols = [col for col in columns_to_drop if col in df.columns]
     if existing_cols:
         df = df.drop(columns=existing_cols)
-
+    
     return df
